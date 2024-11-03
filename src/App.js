9f180005439897c9e01e3974d142b8e41ef4b9fe
HaseethVardhan <haseethvardhan01@gmail.com>
2024-11-03 12:25:10 +0530

    created routes and paths

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-
-
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home.js';
+import Signin from './pages/signin.js';
+import Signup from './pages/signup.js';
+import Profile from './pages/profile.js';
+import About from './pages/about.js';
+ 
 function App() {
   return (
-    <h1 className='text-yellow-400'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-in' element={<Signin/>} />
+        <Route path='/sign-up' element={<Signup/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
